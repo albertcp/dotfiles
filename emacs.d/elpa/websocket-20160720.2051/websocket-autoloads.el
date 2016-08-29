8e473792746e807454c7a25088f4730f68c8bdfb
Alberto Camporredondo <alberto.camporredondop@alumnos.upm.es>
2016-08-29 10:47:38 +0200

    Markdown mode added

---
+++ emacs.d/elpa/websocket-20160720.2051/websocket-autoloads.el
@@ -0,0 +1,18 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("websocket-pkg.el" "websocket.el") (22467
+;;;;;;  62650 106879 251000))
+
+;;;***
+
+(provide 'websocket-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; websocket-autoloads.el ends here
