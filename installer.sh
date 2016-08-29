b137373fbd2310a3931b1381fe2358bd21d5d186
Alberto Camporredondo <alberto.camporredondop@alumnos.upm.es>
2016-08-29 10:14:00 +0200

    Tmux config and installer modified

--- installer.sh
+++ installer.sh
@@ -1,8 +1,15 @@
 echo "Creating dotfiles symbolic links"
-ZSH_DIR=$(pwd)/zsh/.zshrc
+ZSH_DIR=$(pwd)
 EMACS_DIR=$(pwd)/emacs.d
 
 echo "Creating zshrc symbolic link..."
+echo "Testing if it's already install"
+cd $HOME
+if [ -L ".zshrc" ]; then
+    echo "Removing previous symbolic links"
+    rm ".zshrc"
+fi
+cd $ZSH_DIR
 ln -s $ZSH_DIR $HOME/.zshrc
 echo "... Done"
 echo "Creating emacs symbolic link..."
