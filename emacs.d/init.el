72ff0ecc556f7926433e9e1e5772d409f6d7476a
Alberto Camporredondo <alberto.camporredondop@alumnos.upm.es>
2016-09-14 12:29:44 +0200

    some irony fixes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -188,3 +188,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(neo-theme (quote classic))
+ '(neo-window-fixed-size nil)
+ '(neo-window-width 35))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
