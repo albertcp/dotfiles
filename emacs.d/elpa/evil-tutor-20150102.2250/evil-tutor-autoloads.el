0dd3e7c90cdd081693ecf2d355c944c1a8d7beaa
Alberto Camporredondo <albertocp95@gmail.com>
2016-10-13 23:27:46 +0200

    Ignored folders added

---
+++ emacs.d/elpa/evil-tutor-20150102.2250/evil-tutor-autoloads.el
@@ -0,0 +1,29 @@
+;;; evil-tutor-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "evil-tutor" "evil-tutor.el" (22527 62360 567269
+;;;;;;  219000))
+;;; Generated autoloads from evil-tutor.el
+
+(autoload 'evil-tutor-start "evil-tutor" "\
+Start a evil-tutor session.
+
+\(fn)" t nil)
+
+(defalias 'evil-tutor-resume 'evil-tutor-start)
+
+;;;***
+
+;;;### (autoloads nil nil ("evil-tutor-pkg.el") (22527 62360 510599
+;;;;;;  235000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evil-tutor-autoloads.el ends here
---
+++ emacs.d/elpa/hl-todo-20161008.1357/hl-todo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hl-todo" "20161008.1357" "highlight TODO and similar keywords" 'nil :url "https://github.com/tarsius/hl-todo" :keywords '("convenience"))
