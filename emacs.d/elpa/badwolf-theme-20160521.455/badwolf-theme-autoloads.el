3f0c200e984805c25be8802dd94ad4defae4f17b
Alberto Camporredondo <albertocp95@gmail.com>
2016-10-13 23:05:06 +0200

    Added spacemacs

--- emacs.d/elpa/badwolf-theme-20160521.455/badwolf-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; badwolf-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "badwolf-theme" "badwolf-theme.el" (22369 3104
-;;;;;;  524822 961000))
-;;; Generated autoloads from badwolf-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("badwolf-theme-pkg.el") (22369 3104 543280
-;;;;;;  569000))
-
-;;;***
-
-(provide 'badwolf-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; badwolf-theme-autoloads.el ends here
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,168 +1,34 @@
-;;; init.el -- My emacs Configuration
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 
-;;; Commentary:
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
-;;; Code:
+;; increase gc-cons-threshold during init
+(setq gc-cons-threshold 100000000)
 
-;;; %%%%%%%%%%%%%%%%%%%%%%%%%% INIT %%%%%%%%%%%%%%%%%%%%%%%%%%%
+(defconst spacemacs-version          "0.200.1" "Spacemacs version.")
+(defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (package-initialize))
-
-;;; %%%%%%%%%%%%%%%%%%%%%%%% UTILITIES %%%%%%%%%%%%%%%%%%%%%%%%
-
-;; - Style
-; Theme
-(load-theme 'badwolf t)
-; Remove GUI toolbar
-(tool-bar-mode -1)
-
-;; Highlight matching parenthesis
-(show-paren-mode 1)
-
-;; Remove scroll bars
-(setq show-paren-delay 0)
-(scroll-bar-mode -1)
-
-;; Replace highlighted text in type
-(delete-selection-mode 1)
-
-;; - Bindkeys
-; show up emacs menus
-(global-set-key (kbd "C-<f1O>") 'menu-bar-open)
-
-;; - Funcionality
-; windmode : change quickly between buffers
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
-
-; move lines easily
-(global-set-key (kbd "M-s <up>") 'move-text-up) ; move-text-up code at the bottom
-(global-set-key (kbd "M-s <down>") 'move-text-down); move-text-down code at bottom
-
-
-;;; %%%%%%%%%%%%%%%%%%%%%% ADDONS CONFIG %%%%%%%%%%%%%%%%%%%%%%
-
-;; speedbar : view files inside emacs
-(require 'sr-speedbar)
-(global-set-key (kbd "C-c a") 'sr-speedbar-toggle)
-; sr-speedbar no refresh
-(custom-set-variables
- '(custom-safe-themes (quote ("c4a784404a2a732ef86ee969ab94ec8b8033aee674cd20240b8addeba93e1612" default)))
- '(inhibit-startup-screen t)
- '(sr-speedbar-auto-refresh nil))
-
-;; auto-complete :: TODO: change to company
-(require 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; yasnippet
-(require 'yasnippet)
-(yas-global-mode 1)
-
-;; auto-complete-c-headers
-(defun my:ac-c-header-init()
-  (require 'auto-complete-c-headers)
-  (add-to-list 'ac-sources 'ac-source-c-headers)
-  (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-unknown-linux-gnu/5.3.0/include"))
- ; (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-unknown-linux-gnu/*/include"))
-
-(add-hook 'c++-mode-hook 'my:ac-c-header-init)
-(add-hook 'c-mode-hook 'my:ac-c-header-init)
-
-;; iedit config
-(define-key global-map (kbd "C-c ,") 'iedit-mode)
-
-;; company-mode
-;(require 'company)
-;(add-hook 'after-init-hook 'global-company-mode)
-; company-mode for Clang
-;(setq company-backends (delete 'company-semantic company-backends))
-;(define-key c-mode  [(tab)] 'company-complete)
-;(define-key c++-mode-map  [(tab)] 'company-complete)
-
-;; irony autocompletion
-;(add-hook 'c++-mode-hook 'irony-mode)
-;(add-hook 'c-mode-hook 'irony-mode)
-;(add-hook 'objc-mode-hook 'irony-mode)
-; replace the completion-at-point' and complete-symbol' bindings in
-; irony-mode's buffers by irony-mode's function
-;(defun my-irony-mode-hook ()
-; (define-key irony-mode-map [remap completion-at-point]
-;    'irony-completion-at-point-async)
-;  (define-key irony-mode-map [remap complete-symbol]
-;    'irony-completion-at-point-async))
-;(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;; flycheck
-(global-flycheck-mode)
-
-
-;;; %%%%%%%%%%%%%%%%%%%%%% FILES %%%%%%%%%%%%%%%%%%%%%
-;; associate .pl as Ciao
-(add-to-list 'auto-mode-alist '("\\.pl$" . ciao-mode))
-
-
-;;; %%%%%%%%%%%%%%%%%%%%%% MODES %%%%%%%%%%%%%%%%%%%%%
-;; ciao-mode: ciao - a prolog interpreter
-(add-to-list 'load-path "~/.emacs.d/ciao/")
-(load "ciao") ;; best not to include the ending “.el” or “.elc”
-
-;; ghc-mod : extension for emacs mode
-;(add-to-list 'load-path "~/.cabal/share/x86_64-linux-ghc-8.0.1/ghc-mod-5.6.0.0/elisp/")
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-
-;;; %%%%%%%%%%%%%%%%%%%%%%  Code %%%%%%%%%%%%%%%%%%%%%
-;; move lines
-(defun move-text-internal (arg)
-  (cond
-   ((and mark-active transient-mark-mode)
-    (if (> (point) (mark))
-        (exchange-point-and-mark))
-    (let ((column (current-column))
-          (text (delete-and-extract-region (point) (mark))))
-      (forward-line arg)
-      (move-to-column column t)
-      (set-mark (point))
-      (insert text)
-      (exchange-point-and-mark)
-      (setq deactivate-mark nil)))
-   (t
-    (let ((column (current-column)))
-      (beginning-of-line)
-      (when (or (> arg 0) (not (bobp)))
-        (forward-line)
-        (when (or (< arg 0) (not (eobp)))
-          (transpose-lines arg)
-          (when (and (eval-when-compile
-                       '(and (>= emacs-major-version 24)
-                             (>= emacs-minor-version 3)))
-                     (< arg 0))
-            (forward-line -1)))
-        (forward-line -1))
-      (move-to-column column t)))))
-
-(defun move-text-down (arg)
-  "Move region (transient-mark-mode active) or current line ARG lines down."
-  (interactive "*p")
-  (move-text-internal arg))
-
-(defun move-text-up (arg)
-  "Move region (transient-mark-mode active) or current line ARG lines up."
-  (interactive "*p")
-  (move-text-internal (- arg)))
-
-
-(provide 'init)
-;;; init.el ends here
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (message (concat "Your version of Emacs (%s) is too old. "
+                     "Spacemacs requires Emacs version %s or above.")
+             emacs-version spacemacs-emacs-min-version)
+  (load-file (concat (file-name-directory load-file-name)
+                     "core/core-load-paths.el"))
+  (require 'core-spacemacs)
+  (spacemacs/init)
+  (spacemacs/maybe-install-dotfile)
+  (configuration-layer/sync)
+  (spacemacs-buffer/display-info-box)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
